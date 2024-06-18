fe4d221ecb252ff8cbc8e397bfd8510afc11456f
Kazuki <kazuki@amamiya.xyz>
2024-06-18 09:20:50 +0900

    Remove `checkArchiveVolume` and change it as `fun checkArchiveIntegrity`

--- src/main/kotlin/archive/Check.kt
+++ src/main/kotlin/archive/Check.kt
@@ -4,9 +4,7 @@ import ArchiveSetPaths
 import Message
 import MessageType
 
-// TODO: Implement checking missing volume
-fun checkArchiveVolume(packagedFilePaths: Array<ArchiveSetPaths>): Message {
-    if (packagedFilePaths.size <= 1)
-        return Pair(MessageType.Warning, "Only one\nArchiveSet")
-    return Pair(MessageType.NoProblem, "No Problem\nwith Archive Volume")
+// TODO: Implement checking integrity of archive
+fun checkArchiveIntegrity(packagedFilePaths: Array<ArchiveSetPaths>): Message {
+    return Pair(MessageType.NoProblem, "No Problem\nwith Archive")
 }
