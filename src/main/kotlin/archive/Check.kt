b2fedbba92d641566d762cbe73e5d0a6fa51cbf6
Kazuki <kazuki@amamiya.xyz>
2024-06-18 10:05:16 +0900

    Add checkArchiveVolume with checkMissingMultiVolume

--- src/main/kotlin/archive/Check.kt
+++ src/main/kotlin/archive/Check.kt
@@ -3,8 +3,42 @@ package archive
 import ArchiveSetPaths
 import Message
 import MessageType
+import util.checkMissingMultiVolumeFile
+import directoryDelimiter
+import Path
 
-// TODO: Implement checking integrity of archive
-fun checkArchiveIntegrity(packagedFilePaths: Array<ArchiveSetPaths>): Message {
-    return Pair(MessageType.NoProblem, "No Problem\nwith Archive")
+// TODO: Implement checking missing volume or integrity of archive
+fun checkArchiveVolume(packagedFilePaths: Array<ArchiveSetPaths>): Message {
+    if (packagedFilePaths.size <= 1)
+        return Pair(MessageType.Warning, "Only one\nArchiveSet")
+    val paths = mutableListOf<Path>()
+    for (archiveSetPaths in packagedFilePaths) {
+        for (archivePaths in archiveSetPaths) {
+            for (aPath in archivePaths) {
+                paths.add(aPath.joinToString(separator = directoryDelimiter))
+            }
+        }
+    }
+    val checks = checkMissingMultiVolumeFile(paths)
+    val missings = mutableListOf<Path>()
+    val corrupts = mutableListOf<Path>()
+    var mcPaths = ""
+    var isMissing = false
+    for (check in checks) {
+        if (check.isMissing) {
+            isMissing = true
+            for (missing in check.missingVolumes) {
+                missings.add(missing)
+                mcPaths += missing + "\n"
+            }
+            for (corrupted in check.corruptedVolumes) {
+                corrupts.add(corrupted)
+                mcPaths += corrupted + "\n"
+            }
+        }
+    }
+    if (isMissing) {
+        return Pair(MessageType.Critical, "Missing volume\n${mcPaths}")
+    }
+    return Pair(MessageType.NoProblem, "No Problem\nwith Archive Volume")
 }
